4688b0ff47c9c37c85f3b1bd6b6b78ee9edd9e04
Steliyan Sabchev <ssabchev87@gmail.com>
2019-06-18 18:59:02 +0300

    Browser History Update

--- 1. STACKS AND QUEUES/6. Hot Potato/HotPotato.java
+++ 1. STACKS AND QUEUES/6. Hot Potato/HotPotato.java
@@ -17,16 +17,31 @@ public class HotPotato {
         String[] data = reader.readLine().split(" ");
         ArrayDeque<String> queue = new ArrayDeque<>();
         Collections.addAll(queue, data);
-
         int n = Integer.parseInt(reader.readLine());
+
+        int cycleCount = 0;
+
         while (queue.size() > 1){
             for (int i = 1; i < n; i++) {
                 queue.offer(queue.poll());
             }
+            count++;
+            if (count )
 
             System.out.println("Removed " + queue.poll());
         }
 
         System.out.println("Last is " + queue.poll());
+
+
+
+        int counter = 0;
+        if (cycleCount / 1 = )
+
+
     }
+
+
+
+
 }
