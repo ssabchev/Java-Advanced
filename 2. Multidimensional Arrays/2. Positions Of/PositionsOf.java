c8c8d50ab8c57d454e898e90094dce4211b8da8c
Steliyan Sabchev <ssabchev87@gmail.com>
2019-07-11 17:00:55 +0300

    Positions Of

---
+++ 2. Multidimensional Arrays/2. Positions Of/PositionsOf.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class PositionsOf {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        System.in));
+
+        int[] dim = Arrays.stream(reader.readLine().split("\\s+"))
+                .mapToInt(e -> Integer.parseInt(e)).toArray();
+
+        int[][] matrix = new int[dim[0]][dim[1]];
+
+        for (int row = 0; row < matrix.length; row++) {
+            String[] line = reader.readLine().split("\\s+");
+            
+            for (int col = 0; col < line.length; col++) {
+            int value = Integer.parseInt(line[col]);
+
+            matrix[row][col] = value;
+            }
+        }
+        
+        int searchNumber = Integer.parseInt(reader.readLine());
+
+        for (int row = 0; row < matrix.length; row++) {
+            int[] ints = matrix[row];
+            for (int col = 0; col < ints.length; col++) {
+                int value = ints[col];
+                if (value == searchNumber) {
+                    System.out.println(row + " " + col);
+                }
+            }
+        }
+    }
+}
