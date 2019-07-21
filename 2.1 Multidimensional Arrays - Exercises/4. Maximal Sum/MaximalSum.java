d5ad6c124855e96d6e9423cce9bd8905dc4d0f6a
Steliyan Sabchev <ssabchev87@gmail.com>
2019-07-21 18:07:21 +0300

    Fixed bugs

--- 2.1 Multidimensional Arrays - Exercises/4. Maximal Sum/MaximalSum.java
+++ 2.1 Multidimensional Arrays - Exercises/4. Maximal Sum/MaximalSum.java
@@ -4,17 +4,10 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class MaximalSum {
-
-    private static int[][] matrix;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader reader =
-                new BufferedReader(
-                        new InputStreamReader(
-                                System.in
-                        )
-                );
-
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        System.in));
         String[] input = reader.readLine().split("\\s+");
         int rows = Integer.parseInt(input[0]);
         int cols = Integer.parseInt(input[1]);
@@ -23,14 +16,20 @@ public class MaximalSum {
         int rowIndex = -1;
         int colIndex = -1;
 
+        int[][] matrix = new int[rows][cols];
+
+
         for (int row = 0; row < rows; row++) {
-            matrix[row] = Arrays.stream(reader.readLine().split("\\s+"))
+            int[] data = Arrays.stream(reader.readLine().split("\\s+"))
                     .mapToInt(Integer::parseInt).toArray();
+            for (int col = 0; col < cols; col++) {
+                matrix[row][col] = data[col];
+            }
         }
 
         for (int row = 1; row < rows - 1; row++) {
             for (int col = 1; col < cols - 1; col++) {
-                int currnetSum = getmatrixSum(row, col);
+                int currnetSum = getmatrixSum(matrix, row, col);
                 if (currnetSum > maxSum) {
                     maxSum = currnetSum;
                     rowIndex = row;
@@ -40,19 +39,18 @@ public class MaximalSum {
         }
 
         System.out.println("Sum = " + maxSum);
-        printMatrixByGivenCell(rowIndex, colIndex);
+        printMatrixByGivenCell(matrix, rowIndex, colIndex);
 
     }
-
-    private static void printMatrixByGivenCell(int rowIndex, int colIndex) {
+    private static void printMatrixByGivenCell(int[][] matrix, int rowIndex, int colIndex) {
         if (rowIndex == -1 || colIndex == -1) {
             System.out.println("result not found!");
         } else {
             int beginRow = rowIndex - 1;
             int beginCol = colIndex - 1;
 
-            for (int r = beginRow; r < 3; r++) {
-                for (int c = beginCol; c < 3; c++) {
+            for (int r = beginRow; r < beginRow + 3; r++) {
+                for (int c = beginCol; c < beginCol + 3; c++) {
                     System.out.print(matrix[r][c] + " ");
                 }
                 System.out.println();
@@ -60,7 +58,7 @@ public class MaximalSum {
         }
     }
 
-    private static int getmatrixSum(int row, int col) {
+    private static int getmatrixSum(int[][] matrix, int row, int col) {
         int sum = 0;
 
         sum += matrix[row][col];
@@ -75,14 +73,4 @@ public class MaximalSum {
 
         return sum;
     }
-
-//    public static int returnMatrixSum (int[][] matrix) {
-//        int sum = 0;
-//        for (int row = 0; row < matrix.length; row++) {
-//            for (int col = 0; col < matrix[row].length; col++) {
-//                sum += matrix[row][col];
-//            }
-//        }
-//        return sum;
-//    }
 }
