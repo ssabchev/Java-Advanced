c8fff1311945d71e53dca8118516b5fc0287c819
Steliyan Sabchev <ssabchev87@gmail.com>
2019-07-24 14:37:55 +0300

    Voina

---
+++ 3. Sets and Maps Advanced/3. Voina/Voina.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Voina {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Set<Integer> firstPlayer = getPlayerCards(scanner.nextLine());
+        Set<Integer> secondPlayer = getPlayerCards(scanner.nextLine());
+
+        for (int round = 0; round < 50; round++) {
+            if (firstPlayer.isEmpty() || secondPlayer.isEmpty()) break;
+
+            Iterator<Integer> firstPlayerIterator = firstPlayer.iterator();
+            Iterator<Integer> secondPlayerIterator = secondPlayer.iterator();
+
+            int firstPlayerCard = firstPlayerIterator.next();
+            firstPlayer.remove(firstPlayerCard);
+
+            int secondPlayerCard = secondPlayerIterator.next();
+            secondPlayer.remove(secondPlayerCard);
+
+            if (firstPlayerCard > secondPlayerCard) {
+                firstPlayer.add(firstPlayerCard);
+                firstPlayer.add(secondPlayerCard);
+            } else if (firstPlayerCard < secondPlayerCard) {
+                secondPlayer.add(firstPlayerCard);
+                secondPlayer.add(secondPlayerCard);
+            } else {
+                firstPlayer.add(firstPlayerCard);
+                secondPlayer.add(secondPlayerCard);
+            }
+        }
+
+        if (firstPlayer.size() > secondPlayer.size()) {
+            System.out.println("First Player Win!");
+        } else if (firstPlayer.size() < secondPlayer.size()) {
+            System.out.println("Second Player Win!");
+        } else {
+            System.out.println("Draw!");
+        }
+    }
+
+    static LinkedHashSet<Integer> getPlayerCards(String input) {
+        LinkedHashSet<Integer> result = new LinkedHashSet<>();
+        Arrays.stream(input.split("//s+"))
+                .mapToInt(Integer::parseInt)
+                .forEach(result::add);
+        return result;
+    }
+}
